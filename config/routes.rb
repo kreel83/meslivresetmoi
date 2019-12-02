42399426d9357411f3fa11826fc5905e18f01dad
Marc BORGNA <marc.borgna@gmail.com>
2019-12-02 23:08:16 +0100

    search implant

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'books/search', to: 'searchs#index', as: :search
   get 'books/store', to: 'searchs#store', as: :store
+  get 'books/index', to: 'books#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
