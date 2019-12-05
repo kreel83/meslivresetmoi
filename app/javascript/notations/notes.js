523bdd36c3bb1ebc3dafb874765f523bd493d144
Marc BORGNA <marc.borgna@gmail.com>
2019-12-05 11:44:05 +0100

    test

--- app/javascript/notations/notes.js
+++ app/javascript/notations/notes.js
@@ -1,7 +1,7 @@
 const range_init = () => {
   $('.boite').on('mouseover',function() {
     const id = $(this).parent().parent().attr('id')
-    console.log(id)
+
     $('#'+id+' .boite').removeClass("actived").addClass('desactived')
     const pos = $(this).data('id')
     for (let i=0; i<pos; i++) {
