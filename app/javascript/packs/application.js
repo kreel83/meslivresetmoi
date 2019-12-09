0f6bec08e93f1254dfbe9935fe55ff7a03cd5d5e
Marc BORGNA <marc.borgna@gmail.com>
2019-12-09 15:06:40 +0100

    any stuffs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,13 @@
 import "bootstrap";
 import { searchfct } from '../search/search'
-import { range_init } from '../notations/notes'
+import { range_init} from '../notations/notes'
 import { definehidden } from '../books/definehidden'
 import { test } from '../ajax/tags'
 searchfct()
 range_init()
 test()
 definehidden()
+
 
 
 $(document).ready(function () {
