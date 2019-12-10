f3a964bd5a445d837131d0d50e34d1bbf0a4ae9d
alexandre belet <belet.alexandre@gmail.com>
2019-12-10 16:43:19 +0100

    CSS REND FOU

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -1,28 +1,25 @@
 class FollowersController < ApplicationController
-    def index
-      @followers = Follower.where(user: current_user)
-      @f = []
-      @followers.each do |fol|
-        @f << User.find(fol.follower)
-      end
-
+  def index
+    @followers = Follower.where(user: current_user)
+    @f = []
+    @followers.each do |fol|
+      @f << User.find(fol.follower)
     end
+  end
 
-    def create
-      @user = User.find(params[:user])
-      @follow = Follower.new
-      @follow.user = current_user
-      @follow.follower = @user.id
+  def create
+    @user = User.find(params[:user])
+    @follow = Follower.new
+    @follow.user = current_user
+    @follow.follower = @user.id
+    @follow.save
+    redirect_to view_followers_path
+  end
 
-      @follow.save
-      redirect_to view_followers_path
-    end
-
-    def destroy
-      @user = User.find(params[:user])
-      @f = Follower.where(user: current_user, follower: @user.id).first
-      Follower.destroy(@f.id)
-      redirect_to view_followers_path
-    end
-
+  def destroy
+    @user = User.find(params[:user])
+    @f = Follower.where(user: current_user, follower: @user.id).first
+    Follower.destroy(@f.id)
+    redirect_to view_followers_path
+  end
 end
