fb988b3e1efac65dac64254d28c6de80d6b624df
Marc BORGNA <marc.borgna@gmail.com>
2019-12-11 15:40:22 +0100

    debug

--- app/controllers/notations_controller.rb
+++ app/controllers/notations_controller.rb
@@ -29,9 +29,12 @@ class NotationsController < ApplicationController
     @other_reading = Reading.find(params[:id])
     @other_notes = @other_reading.notation.split('/')
 
+
     livre = @other_reading.livre
     @my_reading = Reading.where(user: current_user).where(livre: livre)[0]
-    @my_notes = @my_reading.notation.split('/')
+    if !@my_reading.notation.nil?
+      @my_notes = @my_reading.notation.split('/')
+    end
 
   end
 
